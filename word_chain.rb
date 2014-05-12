8919d7f941d9f401284f4686c5fe1faefae883ef
Matthew Short <short.matthew.f@gmail.com>
2014-05-11 20:06:22 -0400

    working word chainer class, complete with pretty printing of chains

--- word_chain.rb
+++ word_chain.rb
@@ -19,6 +19,7 @@ class Node
 
   def add_child(child_node)
     child_node.parent = self unless children.include?(child_node)
+
     child_node
   end
 
@@ -81,6 +82,31 @@ class WordChainer
   def initialize(start_word)
     @start_word = start_word
     @working_words = create_dictionary.select { |word| word.length == start_word.length }
+    @root = build_tree
+  end
+
+
+  # here we build an entire tree for the input word.  
+  # this is inefficient when we're just searching for a specific word
+  # path, but it allows us to search for any word path once it's over
+  def build_tree
+    root = Node.new(start_word)
+    node_queue = [root]
+
+    until node_queue.empty? || self.working_words.empty?
+      current_node = node_queue.shift
+      current_word = current_node.value
+
+      neighbors = adjacent_words(current_word, self.working_words)
+
+      neighbors.each do |word|
+        node_queue << current_node.add_child(Node.new(word))
+      end
+
+      self.working_words -= neighbors
+    end
+
+    root
   end
 
   def create_dictionary
@@ -101,7 +127,13 @@ class WordChainer
     end.count == 1
   end
 
+  # chain to 'pretty-prints' our word chains
   def chain_to(end_word)
-
+    end_node = @root.dfs(end_word)
+    if end_node
+      end_node.path_to_root.map { |node| node.value }.join(" => ")
+    else
+      nil
+    end
   end
 end
